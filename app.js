f4c6d9b80c85c4c0e482cd8bb9a4ca30b6350205
Thomas Chester <thomcchester@gmail.com>
2016-02-28 21:52:41 -0600

    fifth

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
 
 $(document).ready(function(){
-    runAll();
+    runAll(); //I decided to make it so that it was all in a function here
+              // mainly just to see.
 });
 
-function runAll(){
-  $("#employeeForm").on("submit", function(event){
+function runAll(){ // This is my function that runs everything in the document ready
+  $("#employeeForm").on("submit", function(event){ //this function is addapted from
+    //the car array one
     event.preventDefault();
 
-    var employeeValues = {};
+    var employeeValues = {}; //This makes objects
 
     $.each($("#employeeForm").serializeArray(), function(i, field){
-      employeeValues[field.name] = field.value;
+      employeeValues[field.name] = field.value; //taken directly from scott's
+      //example
     });
     $("#employeeForm").find("input[type=text]").val("");
 
     employeeArray.push(employeeValues);
-    $(".container2").empty();
-    eachIndividual();
-    //salaryBurnMonthly();
+    $(".container2").empty(); //I made a second container
+    eachIndividual();//this runs each individual student
     $(".buttonDelete").on("click", runDelete);
   });
-  $(".MBR").on("click", salaryBurnMonthly);
+  $(".MBR").on("click", salaryBurnMonthly);// this is for the Monthly
+  //burn rate thing
 };
-function runDelete(){
-  var checker = parseInt(this.id);
-  employeeArray.splice(checker,1);
-  $(this).parent().parent().remove();
-  $(".body").append('<div class="container2"></div>')
+function runDelete(){ // delete function
+  var checker = parseInt(this.id);//takes the id number and turns to
+  // to a number and find the employeeArray
+  employeeArray.splice(checker,1);//takes array index
+  $(this).parent().parent().remove(); //removes containter2 it from array
+  $(".body").append('<div class="container2"></div>')//reputs containter2 in
+
 
 }
 
-var employeeArray=[];
-
-function removeEmployee(){
-  //console.log($(this).parent())
-  //$(this).parent().remove();
-}
+var employeeArray=[];//array of employees
 
 function salaryBurnMonthly(){
   var totMonBurn = 0;
